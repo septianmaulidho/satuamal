d6824dc62a0ae09b9c198ed54bd32bea947b67c2
JatraMaya <ahmad.f.iksan@gmail.com>
2021-02-12 10:58:51 +0800

    Make findByEmail method to lookup existing user

--- src/main/java/com/testing/LastProject/Repository/UserRepository.java
+++ src/main/java/com/testing/LastProject/Repository/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.testing.LastProject.model.User;
 
-public interface UserRepository extends JpaRepository<User,String>{
-
+public interface UserRepository extends JpaRepository<User,Integer>{
+	User findByEmail(String email); // Add method to lookup existing user by email
 }
