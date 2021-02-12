0beeb10d35ee7aa4b67062b93f37e9ccb277afaf
JatraMaya <ahmad.f.iksan@gmail.com>
2021-02-12 11:25:13 +0800

    Create new register method on UserService interface

--- src/main/java/com/testing/LastProject/Service/UserService.java
+++ src/main/java/com/testing/LastProject/Service/UserService.java
@@ -2,7 +2,9 @@ package com.testing.LastProject.Service;
 
 import com.testing.LastProject.Response.UserResponse;
 import com.testing.LastProject.payload.UpdateUserPayload;
+import com.testing.LastProject.payload.UserPayload;
 
 public interface UserService {
+	public UserResponse register(UserPayload userPayload);
 	public UserResponse update(String id, UpdateUserPayload updateuserPayload);
 }
