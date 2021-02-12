51f3861db9dd419e2eef6d8c974b278a9a3cd463
JatraMaya <ahmad.f.iksan@gmail.com>
2021-02-13 00:14:37 +0800

    Implement tokenization for register and login response

--- src/main/java/com/testing/LastProject/Controller/AuthController.java
+++ src/main/java/com/testing/LastProject/Controller/AuthController.java
@@ -1,5 +1,8 @@
 package com.testing.LastProject.Controller;
 
+import java.security.Key;
+import java.security.Security;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,13 +15,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.testing.LastProject.ErrorHandler.AuthException;
+import com.testing.LastProject.JWTconfig.JwtConstant;
 import com.testing.LastProject.Response.UserResponse;
 import com.testing.LastProject.Service.UserService;
 import com.testing.LastProject.payload.UserPayload;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
 @RestController
 @RequestMapping("/api/user")
 public class AuthController {
+	
+	public Map<String, String> generateJWTToken(UserResponse userResponse){
+		long timestamp = System.currentTimeMillis();
+		Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+		@SuppressWarnings("deprecation")
+		String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, key)
+				.setIssuedAt(new Date(timestamp))
+				.setExpiration(new Date(timestamp + JwtConstant.TOKEN_VALIDITY))
+				.claim("userId", userResponse.getId())
+				.claim("name", userResponse.getName())
+				.claim("email", userResponse.getEmail())
+				.claim("phoneNumber", userResponse.getPhoneNumber())
+				.claim("role", userResponse.getRole())
+				.claim("status", userResponse.getStatus())
+				.compact();
+		Map<String, String> map = new HashMap<>();
+		map.put("token", token);
+		return map;
+	}
 
 	@Autowired
 	UserService userService;
@@ -26,16 +53,16 @@ public class AuthController {
 	@PostMapping("/register")
 	public ResponseEntity<?> register(@RequestBody UserPayload userPayload){
 		UserResponse user = userService.register(userPayload);
-		return new ResponseEntity<>(user, HttpStatus.OK);
+		return new ResponseEntity<>(generateJWTToken(user), HttpStatus.OK);
 	}
 	
 	@PostMapping("/login")
 	public ResponseEntity<?> login(@RequestBody UserPayload userPayload) throws AuthException {
 		try {
 			UserResponse user = userService.login(userPayload);
-			Map<String, String> map = new HashMap<>();
-			map.put("message", "login succesfull");
-			return new ResponseEntity<>(map, HttpStatus.OK);
+//			Map<String, String> map = new HashMap<>();
+//			map.put("message", "login succesfull");
+			return new ResponseEntity<>(generateJWTToken(user), HttpStatus.OK);
 		}catch (Exception e) {
 			Map<String, String> map = new HashMap<>();
 			map.put("message", "user unauthorized");
