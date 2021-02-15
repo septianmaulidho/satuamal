e23e0c3aafdc0d3efdd7cc054b9ad3f8a7fe0998
ridhoajibx <ridhoajibx@gmail.com>
2021-02-16 00:21:12 +0700

    [ridho] - clear maven

--- src/main/java/com/glints/satuamal/repository/CityRepo.java
+++
@@ -1,9 +0,0 @@
-package com.glints.satuamal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.glints.satuamal.model.City;
-
-public interface CityRepo extends JpaRepository<City, String> {
-
-}
--- src/main/java/com/glints/satuamal/repository/RecipientRepo.java
+++
@@ -1,9 +0,0 @@
-package com.glints.satuamal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.glints.satuamal.model.Recipient;
-
-public interface RecipientRepo extends JpaRepository<Recipient, String>{
-
-}
